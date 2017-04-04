62c9d676a6d3e9ac3520365396822ab482135f95
Oz N Tiram <oz.tiram@gmail.com>
2017-04-04 22:09:48 +0200

    Add a more consitent OrderedDict based class

--- ttldict/__init__.py
+++ ttldict/__init__.py
@@ -1,17 +1,10 @@
-"""
-TTL dictionary
-
-Tricks / features:
- - calling len() will remove expired keys
- - __repr__() might show expired values, doesn't remove expired ones
-"""
-
-__all__ = ['TTLDict']
-__version__ = '0.0.4'
-
 from collections import MutableMapping
+from collections import OrderedDict
 from threading import RLock
 import time
+
+__all__ = ['TTLDict', 'TTLOrderedDict']
+__version__ = '0.1.0'
 
 
 class TTLDict(MutableMapping):
@@ -26,12 +19,11 @@ class TTLDict(MutableMapping):
         self.update(*args, **kwargs)
 
     def __repr__(self):
-        self._purge()
         return '<TTLDict@%#08x; ttl=%r, v=%r;>' % (
             id(self), self._default_ttl, self._values)
 
     def set_ttl(self, key, ttl, now=None):
-        """Set TTL for the given key"""
+        """ Set TTL for the given key """
         if now is None:
             now = time.time()
         with self._lock:
@@ -39,7 +31,7 @@ class TTLDict(MutableMapping):
             self._values[key] = (now + ttl, value)
 
     def get_ttl(self, key, now=None):
-        """Return remaining TTL for a key"""
+        """ Return remaining TTL for a key """
         if now is None:
             now = time.time()
         with self._lock:
@@ -47,37 +39,35 @@ class TTLDict(MutableMapping):
             return expire - now
 
     def expire_at(self, key, timestamp):
-        """Set the key expire timestamp"""
+        """ Set the key expire timestamp """
         with self._lock:
             _expire, value = self._values[key]
             self._values[key] = (timestamp, value)
 
-    def is_expired(self, key, now=None):
-        """ Check if key has expired, and return it if so"""
+    def is_expired(self, key, now=None, remove=False):
+        """ Check if key has expired """
         with self._lock:
             if now is None:
                 now = time.time()
-
             expire, _value = self._values[key]
-
-            if expire:
-                if expire < now:
-                    return key
-
-    def _purge(self):
-        _remove = [key for key in self._values.keys() if self.is_expired(key)]  # noqa
-        [self._values.pop(key) for key in _remove]
+            if expire is None:
+                return False
+            expired = expire < now
+            if expired and remove:
+                self.__delitem__(key)
+            return expired
 
     def __len__(self):
         with self._lock:
-            self._purge()
+            for key in self._values.keys():
+                self.is_expired(key, remove=True)
             return len(self._values)
 
     def __iter__(self):
         with self._lock:
-            self._purge()
             for key in self._values.keys():
-                yield key
+                if not self.is_expired(key, remove=True):
+                    yield key
 
     def __setitem__(self, key, value):
         with self._lock:
@@ -93,7 +83,89 @@ class TTLDict(MutableMapping):
 
     def __getitem__(self, key):
         with self._lock:
+            self.is_expired(key, remove=True)
+            return self._values[key][1]
+
+
+class TTLOrderedDict(OrderedDict):
+    """
+    OrderedDict with TTL
+    Extra args and kwargs are passed to initial .update() call
+    """
+    def __init__(self, default_ttl, *args, **kwargs):
+        self._default_ttl = default_ttl
+        self._lock = RLock()
+        super().__init__()
+        self.update(*args, **kwargs)
+
+    def __repr__(self):
+        return '<TTLDict@%#08x; ttl=%r, v=%r;>' % (
+            id(self), self._default_ttl, super().__repr__())
+
+    def set_ttl(self, key, ttl, now=None):
+        """Set TTL for the given key"""
+        if now is None:
+            now = time.time()
+        with self._lock:
+            value = self[key]
+            super().__setitem__(key, (now + ttl, value))
+
+    def get_ttl(self, key, now=None):
+        """Return remaining TTL for a key"""
+        if now is None:
+            now = time.time()
+        with self._lock:
+            expire, _value = super().__getitem__(key)
+            return expire - now
+
+    def expire_at(self, key, timestamp):
+        """Set the key expire timestamp"""
+        with self._lock:
+            value = self.__getitem__(key)
+            super().__setitem__(key,  (timestamp, value))
+
+    def is_expired(self, key, now=None):
+        """ Check if key has expired, and return it if so"""
+        with self._lock:
+            if now is None:
+                now = time.time()
+
+            expire, _value = super().__getitem__(key)
+
+            if expire:
+                if expire < now:
+                    return key
+
+    def _purge(self):
+        _keys = list(super().__iter__())
+        _remove = [key for key in _keys if self.is_expired(key)]  # noqa
+        [self.__delitem__(key) for key in _remove]
+
+    def __iter__(self):
+        """make a snapshot iterator of keys.
+        If you use this iterator it does not purge keys while you
+        iterate.
+        """
+        with self._lock:
+            for key in super().__iter__():
+                if not self.is_expired(key):
+                    yield key
+
+    def __setitem__(self, key, value):
+        with self._lock:
+            if self._default_ttl is None:
+                expire = None
+            else:
+                expire = time.time() + self._default_ttl
+            super().__setitem__(key,  (expire, value))
+
+    def __delitem__(self, key):
+        with self._lock:
+            super().__delitem__(key)
+
+    def __getitem__(self, key, default=None):
+        with self._lock:
             if self.is_expired(key):
                 self.__delitem__(key)
             else:
-                return self._values[key][1]
+                return super().__getitem__(key)[1]
--- ttldict/test_ttldict.py
+++ ttldict/test_ttldict.py
@@ -3,7 +3,7 @@ Unit tests for TTLDict
 """
 
 from unittest import TestCase
-from ttldict import TTLDict
+from ttldict import TTLOrderedDict, TTLDict
 import time
 
 
@@ -77,6 +77,86 @@ class TTLDictTest(TestCase):
         """ Test is_expired() call """
         now = time.time()
         ttl_dict = TTLDict(60, a=1, b=2)
+        self.assertFalse(ttl_dict.is_expired('a'))
+        self.assertFalse(ttl_dict.is_expired('a', now=now))
+        self.assertTrue(ttl_dict.is_expired('a', now=now+61))
+
+        # remove=False, so nothing should be gone
+        self.assertEqual(len(ttl_dict), 2)
+
+
+class TTLOrderedDictTest(TestCase):
+    """ TTLOrderedDict tests """
+    def test_update_no_ttl(self):
+        """ Test update() call """
+        ttl_dict = TTLOrderedDict(None)
+        orig_dict = {'hello': 'world', 'intval': 3}
+        ttl_dict.update(orig_dict)
+        self.assertEqual(sorted(orig_dict.items()), sorted(ttl_dict.items()))
+
+    def test_purge_clears_expired_items(self):
+        """ Test that calling _purge() removes expired items """
+        ttl_dict = TTLOrderedDict(1, a=1, b=2)
+        self.assertEqual(sorted(ttl_dict.keys()), sorted(['a', 'b']))
+        time.sleep(2)
+        ttl_dict._purge()
+        self.assertEqual(len(ttl_dict), 0)
+        self.assertEqual(list(ttl_dict.keys()), [])
+
+    def test_expire_at(self):
+        """ Test expire_at """
+        ttl_dict = TTLOrderedDict(60)
+        ttl_dict['a'] = 100
+        ttl_dict['b'] = 123
+        self.assertEqual(ttl_dict['a'], 100)
+        self.assertEqual(ttl_dict['b'], 123)
+        self.assertEqual(len(ttl_dict), 2)
+        ttl_dict.expire_at('a', time.time())
+        self.assertIsNone(ttl_dict['a'])
+        self.assertEqual(len(ttl_dict), 1)
+        self.assertEqual(ttl_dict['b'], 123)
+
+    def test_set_ttl_get_ttl(self):
+        """ Test set_ttl() and get_ttl() """
+        ttl_dict = TTLOrderedDict(120, foo=3, bar=None)
+        self.assertEqual(sorted(ttl_dict), ['bar', 'foo'])
+        self.assertEqual(ttl_dict['foo'], 3)
+        self.assertEqual(ttl_dict['bar'], None)
+        self.assertEqual(len(ttl_dict), 2)
+        ttl_dict.set_ttl('foo', 3)
+        ttl_foo = ttl_dict.get_ttl('foo')
+        self.assertTrue(ttl_foo <= 3.0)
+        ttl_bar = ttl_dict.get_ttl('bar')
+        self.assertTrue(ttl_bar - ttl_foo > 100)
+
+    def test_set_ttl_key_error(self):
+        """ Test that set_ttl() raises KeyError """
+        ttl_dict = TTLOrderedDict(60)
+        self.assertRaises(KeyError, ttl_dict.set_ttl, 'missing', 10)
+
+    def test_get_ttl_key_error(self):
+        """ Test that get_ttl() raises KeyError """
+        ttl_dict = TTLOrderedDict(60)
+        self.assertRaises(KeyError, ttl_dict.get_ttl, 'missing')
+
+    def test_iter_empty(self):
+        """ Test that empty TTLOrderedDict can be iterated """
+        ttl_dict = TTLOrderedDict(60)
+        for key in ttl_dict:
+            self.fail("Iterating empty dictionary gave a key %r" % (key,))
+
+    def test_iter(self):
+        """ Test that TTLOrderedDict can be iterated """
+        ttl_dict = TTLOrderedDict(60)
+        ttl_dict.update(zip(range(10), range(10)))
+        self.assertEqual(len(ttl_dict), 10)
+        for key in ttl_dict:
+            self.assertEqual(key, ttl_dict[key])
+
+    def test_is_expired(self):
+        """ Test is_expired() call """
+        now = time.time()
+        ttl_dict = TTLOrderedDict(60, a=1, b=2)
         self.assertFalse(ttl_dict.is_expired('a'))
         self.assertFalse(ttl_dict.is_expired('a', now=now))
         self.assertTrue(ttl_dict.is_expired('a', now=now+61))
