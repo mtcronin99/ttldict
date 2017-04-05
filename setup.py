cb30e3f3273be4feba3828fba6d64636b749b396
Oz N Tiram <oz.tiram@mobilityhouse.com>
2017-04-05 13:42:12 +0200

    add installer and .coveragerc

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+
+setup(name='ttldict',
+      version='0.2.1',
+      description="Dictionary with auto-expiring values for caching purposes",
+      long_description=long_description,
+      author='Oz Tiram',
+      author_email='oztiram@mobilityhouse.com',
+      url='https://github.com/mobilityhouse/ttldict',
+      license='MIT',
+      packages=find_packages(exclude=['tests']),
+      include_package_data=True,
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5'],
+      zip_safe=True,
+      extras_require={'test': ['pytest', 'coverage']})
