4bc48bcf6c708e46d34cc1452a8d845d399de4fb
Oz N Tiram <oz.tiram@mobilityhouse.com>
2017-05-17 12:39:46 +0200

    Bump version 0.2.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 
 setup(name='ttldict',
-      version='0.2.6',
+      version='0.2.7',
       description="Dictionary with auto-expiring values for caching purposes",
       long_description=long_description,
       author='Oz Tiram',
